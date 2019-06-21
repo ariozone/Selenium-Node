89c3cd8c0c4fde57bb4e06d77de0d695c3c0e3b4
Ario Tamaddoni <ariozone6@gmail.com>
2019-06-21 15:56:44 -0700

    Used explicit xpath to find input element and send input data to it.

--- rsvp/test/index.js
+++ rsvp/test/index.js
@@ -1,4 +1,14 @@
 const selenium = require("selenium-webdriver")
 const driver = new selenium.Builder().forBrowser("chrome").build()
 const { url1: url } = require("../../config.json")
+const By = selenium.By
+
 driver.get(url)
+
+const inputField = driver.findElement(
+  By.xpath("/html/body/div/header/form/input")
+)
+
+inputField.sendKeys("Found this element with an explicit xpath")
+
+// driver.close()
