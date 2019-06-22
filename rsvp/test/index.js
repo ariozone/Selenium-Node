7a317d1980cd2f68c4f3a68d5b528212e886e24d
Ario Tamaddoni <ariozone6@gmail.com>
2019-06-22 11:55:11 -0700

    Added test data and and created elements using test data.

--- rsvp/test/index.js
+++ rsvp/test/index.js
@@ -2,6 +2,7 @@ const selenium = require("selenium-webdriver")
 const By = selenium.By
 const driver = new selenium.Builder().forBrowser("chrome").build()
 const { url1: url } = require("../../config.json")
+const invitees = require("./test-data")()
 
 driver.get(url)
 
@@ -12,18 +13,17 @@ const locators = {
   toggleVisiblity: By.css(".main > div input")
 }
 
-function addInvitee(name) {
+async function addInvitee(name) {
   driver.findElement(locators.inviteeNameField).sendKeys(name)
-  driver.findElement(locators.inviteeForm)
-  driver.findElement(locators.formSubmit).click()
+  // driver.findElement(locators.inviteeForm)
+  await driver.findElement(locators.formSubmit).click()
 }
 function toggleVisiblity() {
   driver.findElement(locators.toggleVisiblity).click()
 }
-addInvitee("Ario")
-addInvitee("Sam")
-addInvitee("Tom")
 
-toggleVisiblity()
+invitees.forEach(invitee => addInvitee(invitee))
+
+// toggleVisiblity()
 
 // driver.close()
