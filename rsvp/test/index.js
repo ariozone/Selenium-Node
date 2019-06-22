4214054b3f2d4676d348d7836bfbe82ff906ddda
Ario Tamaddoni <ariozone6@gmail.com>
2019-06-22 11:21:50 -0700

    Using css selectors to find and interact with child elements.

--- rsvp/test/index.js
+++ rsvp/test/index.js
@@ -1,27 +1,29 @@
 const selenium = require("selenium-webdriver")
+const By = selenium.By
 const driver = new selenium.Builder().forBrowser("chrome").build()
 const { url1: url } = require("../../config.json")
-const By = selenium.By
 
 driver.get(url)
 
-//Using explicit xpath to find element
-// const inputField = driver.findElement(
-//   By.xpath("/html/body/div/header/form/input")
-// )
-
-async function getAsynchronously() {
-  // const form = driver.findElement(By.id("registrar"))
-  // form.findElement(By.css("input")).sendKeys("Ario")
-  // form.findElement(By.css("button")).click()
-  try {
-    const header = driver.findElement(By.css("h1"))
-    const text = await header.getText()
-    console.log(text)
-  } catch (error) {
-    console.error(error)
-  }
+const locators = {
+  inviteeForm: By.id("registrar"),
+  inviteeNameField: By.name("name"),
+  formSubmit: By.css("button"),
+  toggleVisiblity: By.css(".main > div input")
 }
-getAsynchronously()
+
+function addInvitee(name) {
+  driver.findElement(locators.inviteeNameField).sendKeys(name)
+  driver.findElement(locators.inviteeForm)
+  driver.findElement(locators.formSubmit).click()
+}
+function toggleVisiblity() {
+  driver.findElement(locators.toggleVisiblity).click()
+}
+addInvitee("Ario")
+addInvitee("Sam")
+addInvitee("Tom")
+
+toggleVisiblity()
 
 // driver.close()
