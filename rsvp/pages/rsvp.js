d00730cf0e9307790195562549eef5bc5d3e7a29
Ario Tamaddoni <ariozone6@gmail.com>
2019-06-25 19:00:19 -0700

    Accessing elements using implicit wait.

--- rsvp/pages/rsvp.js
+++ rsvp/pages/rsvp.js
@@ -1,24 +1,14 @@
 const { Browser, By, Key, until } = require("selenium-webdriver")
 const { rsvp: url } = require("../../config.json")
 
-// Define a class to represent the page.
-// Instances of this class will be responsible for controlling WebDriver to do operations on the page.
 class RsvpPage {
-  // The class constructor sets up new instances of the class.
-  // We're going to have the test pass in a browser driver object when creating a page object.
-  // The page object will call methods on the driver object to control the page.
   constructor(driver) {
-    // We'll set the "driver" property of the new object to equal the driver that's passed in.
     this.driver = driver
-    // The locators property will be an object containing locators for the different page
-    // elements we need to find.
+
     this.locators = {
-      // Our first test uses this locator to find the list that we'll populate with invitees.
-      // We'll move the locator from the test to here.
       invitedList: By.id("invitedList"),
-      // Our second tests uses this locator to find the form to register invitees.
-      // We'll move that here, too.
-      registrationForm: By.id("registrar")
+      registrationForm: By.id("registrar"),
+      invitees: By.css("#invitedList li")
     }
   }
 
--- rsvp/test/invitees.js
+++ rsvp/test/invitees.js
@@ -22,14 +22,36 @@ suite(function(env) {
 
     it("has invitee list", async function() {
       // Use the locator from the page object instead.
-      let elements = await driver.findElements(page.locators.invitedList)
+      const elements = await driver.findElements(page.locators.invitedList)
       assert(elements.length > 0)
     })
 
     it("has registration form", async function() {
       // Use the locator from the page object instead.
-      let elements = await driver.findElements(page.locators.registrationForm)
+      const elements = await driver.findElements(page.locators.registrationForm)
       assert(elements.length > 0)
+    })
+
+    // We'll add a test with a description saying the page "loads existing invitations".
+    // We'll set up the test in another asynchronous function.
+    it("loads existing invitations", async function() {
+      // We'll call our driver object's findElements() method. Notice that's "findElements"
+      // PLURAL, not singular - we expect to find multiple matching elements, so this will
+      // return an array of elements. We'll pass findElements() the invitees locator from
+      // our page object, which is set up to find all the "li" elements inside the element
+      // with an ID of "invitedList". findElements() returns a promise, soWe use the "await"
+      // keyword to wait for the promise to resolve, and we store the resulting array in
+      // the "invitees" variable.
+      await driver.manage().setTimeouts({ implicit: 3000 })
+      let invitees = await driver.findElements(page.locators.invitees)
+      // Now, we need to test whether the invitees list loaded correctly. Each element
+      // contains the invitee's name. So I'm going to access the second invitee element,
+      // and get its text using the getText() method. As always, this returns a promise,
+      // so I'll get its resolved value using "await".
+      let text = await invitees[1].getText()
+      // Finally, we need the test itself. I'm going to assert that the element text
+      // includes the string "Craig Dennis".
+      assert(text.includes("Craig Dennis"))
     })
 
     after(async function() {
