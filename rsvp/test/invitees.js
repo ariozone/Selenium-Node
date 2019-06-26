9a6ffbcc3259b73cdd4d584cbd33993c3f40920d
Ario Tamaddoni <ariozone6@gmail.com>
2019-06-25 18:24:02 -0700

    Moved page code to a page object class.

--- rsvp/test/invitees.js
+++ rsvp/test/invitees.js
@@ -1,27 +1,37 @@
-const { rsvp } = require("../../config.json")
-const { Browser, By, Key, until } = require("selenium-webdriver")
 const { suite } = require("selenium-webdriver/testing")
 const assert = require("assert")
-
-const url = rsvp
+// Load the RsvpPage class.
+const RsvpPage = require("../pages/rsvp.js")
 
 suite(function(env) {
-  describe("RSVP site", function() {
+  describe("RSVP site", async function() {
     let driver
+    // Define a variable to hold the page object here so that it stays in scope
+    // for all our tests as well.
+    let page
+
     before(async function() {
-      this.timeout(3000)
       driver = await env.builder().build()
-      await driver.get(url)
+      // Create a new page object that will use our driver object.
+      // Store it in the page variable.
+      page = new RsvpPage(driver)
+      // Instead of calling driver.get() ourselves, we'll let the page object
+      // load the page for us.
+      await page.open()
     })
+
     it("has invitee list", async function() {
-      const elements = await driver.findElements(By.id("invitedList"))
+      // Use the locator from the page object instead.
+      let elements = await driver.findElements(page.locators.invitedList)
       assert(elements.length > 0)
     })
 
     it("has registration form", async function() {
-      const elements = await driver.findElements(By.id("registrar"))
+      // Use the locator from the page object instead.
+      let elements = await driver.findElements(page.locators.registrationForm)
       assert(elements.length > 0)
     })
+
     after(async function() {
       driver.quit()
     })
