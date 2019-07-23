8c0306c46dc7741c790e66e0556a783611173178
Ario Tamaddoni <ariozone6@gmail.com>
2019-07-22 21:09:25 -0700

    Enhanced the webdriver to get text of the elements in library app.

--- library/library.js
+++ library/library.js
@@ -1,7 +1,22 @@
 const webdriver = require("selenium-webdriver")
+require("chromedriver")
 const By = webdriver.By
 const until = webdriver.until
-const driver = new webdriver.Builder().forBrowser("chrome").build()
 const { library_app: url } = require("../config.json")
+const driver = new webdriver.Builder().forBrowser("chrome").build()
 
-driver.get("https://google.com")
+driver.get(url)
+driver.findElement(By.css("input")).sendKeys("user@domain.com")
+driver
+  .findElement(By.css(".btn-lg"))
+  .getText()
+  .then(txt => {
+    console.log(txt)
+  })
+driver.findElements(By.css("nav li")).then(elements => {
+  elements.map(element =>
+    element.getText().then(txt => {
+      console.log(txt)
+    })
+  )
+})
